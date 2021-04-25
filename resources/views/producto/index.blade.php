55fad66e46672d64247193eabccd6efc365963c6
Fanny581 <fannyyulissacruz16@gmail.com>
2021-04-24 22:20:22 -0600

    index

--- resources/views/producto/index.blade.php
+++ resources/views/producto/index.blade.php
@@ -1,74 +1,49 @@
 @extends('adminlte::page')
 
-@section('title', 'Dashboard')
+@section('title', 'produco')
 
 @section('content_header')
+    <h1>Clientes</h1>
 @stop
 
 @section('content')
-
-<div class="card card-primary">
-    <div class="card-header">
-        <h3 class="card-title">Productos</h3>
-    </div>
-    <form>
-        <div class="row">
-            <div class="col-sm-6">
-                <div class="card-body">
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Nombre del Producto</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-                    </div>
-                    
-                    </div>
-                    <div class="col-sm-6">
-                <div class="card-body">
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Codigo de Producto</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-                    </div>
-                    
-                </div>
-            </div>
-            <div class="col-sm-6">
-                <div class="card-body">
-                    
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Precio de Ventas</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-                    </div>
-                    
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Descuento</label>
-                        <input type="" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-                    </div>
-    </form>
-
-                    <div class="form-group">
-                   
-                        </div>
-                    </div>
-                </div>
-            </div>
+    @if (session()->get('success'))
+        <div class="alert alert-success">
+            {{session()->get('success')}}
         </div>
-        <div class="card-footer">
-        <button type="submit" class="btn btn-primary">crear</button>
-        <div>
-    <input type="submit" value="Save">
-  </div>
-        </div>
-         
-    </form>
-</div>
+    @endif
+
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>Nombre del produco</th>
+                <th>codigo del produc</th>
+                <th>precio venta</th>
+                <th>descuento</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($producto as $producto)
+                <tr>
+                <td> {{$producto->id}}</td>
+                    <td> {{$producto->Nombredelproducto}}</td>
+                    <td> {{$producto->codigodelproducto}} </td>
+                    <td> {{$producto->ventadeproduco}} </td>
+                    <td> {{$producto->descuento}} </td>
+                    <td>
+                        <a href="{{route('producto.edit', $producto->id)}}" class="btn btn-success btn-sm">Editar</a>
+                        <form action="{{route('producto.destroy', $producto->id)}}" method="post" style="display: inline-block">
+                            @csrf
+                            @method('DELETE')
+                            <button class="btn btn-danger btn-sm" type="submit">Eliminar</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @stop
 
 @section('css')
-<link rel="stylesheet" href="/css/admin_custom.css">
+    <link rel="stylesheet" href="/css/admin_custom.css">
 @stop
-
-@section('js')
-<script>
-console.log('Hi!');
-</script>
-@stop
-
